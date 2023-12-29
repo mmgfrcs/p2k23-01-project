68b2896b73c213202606f550d53c5382c2af907e
Virya Dharmawan <viryadharmawan@gmail.com>
2023-12-29 18:48:51 +0700

    Version Text
    Adds version text to know which version the game is

---
+++ Assets/Scripts/UI/VersionText.cs
@@ -0,0 +1,16 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class VersionText : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+            var text = GetComponent<TextMeshProUGUI>();
+            text.text = $"{Application.platform}\nVersion {Application.version}";
+        }
+    }
+}
